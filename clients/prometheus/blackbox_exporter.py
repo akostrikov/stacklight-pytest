e59d7ae3e4473648ad1ab383fbf1aaa1bdd0c06a
Dmitry Kalashnik <dkalashnik@mirantis.com>
2017-02-08 19:09:05 +0400

    Add prometheus API and collectors clients

---
+++ clients/prometheus/blackbox_exporter.py
@@ -0,0 +1,14 @@
+from clients import http_client
+
+
+class BlackBoxExporterClient(http_client.HttpClient):
+    def get_probe(self, target, module="http_2xx"):
+
+        params = {
+            "module": module,
+            "target": target
+        }
+
+        _, result = self.get("/probe", params=params)
+
+        return result.splitlines()
